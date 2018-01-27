3f3353e333b058f8b896f5b27b229f4f1e77d9e9
Olmo Cerri <olmo.cerri@gmail.com>
2018-01-26 18:12:26 -0800

    First Commit. Delphes 3.4.1 copied form master and already changed in modules and classes

---
+++ modules/VertexFinderDAClusterizerZT.h
@@ -0,0 +1,66 @@
+#ifndef VertexFinderDAClusterizerZT_h
+#define VertexFinderDAClusterizerZT_h
+
+/** \class VertexFinderDAClusterizerZT
+ *
+ *  Cluster vertices from tracks using deterministic annealing and timing information
+ *
+ *  \authors M. Selvaggi, L. Gray
+ *
+ */
+
+
+#include "classes/DelphesModule.h"
+
+#include <vector>
+
+class TObjArray;
+class TIterator;
+class Candidate;
+
+class VertexFinderDAClusterizerZT: public DelphesModule
+{
+public:
+
+  VertexFinderDAClusterizerZT();
+  ~VertexFinderDAClusterizerZT();
+
+  void Init();
+  void Process();
+  void Finish();
+
+  void clusterize(const TObjArray &tracks, TObjArray &clusters);
+  std::vector< Candidate* > vertices();
+
+private:
+
+  Bool_t fVerbose;
+
+  UInt_t fMaxIterations;
+  Double_t fMinTrackWeight;
+  Bool_t fUseTc;
+
+  Float_t fBetaMax;
+  Float_t fBetaStop;
+  Float_t fBetaPurge;
+
+  Double_t fMinPT;
+
+  Float_t fVertexSpaceSize;
+  Float_t fVertexTimeSize;
+
+  Double_t fCoolingFactor;
+  Double_t fDzCutOff;
+  Double_t fD0CutOff;
+  Double_t fDtCutOff; // for when the beamspot has time
+
+  TObjArray *fInputArray;
+  TIterator *fItInputArray;
+
+  TObjArray *fOutputArray;
+  TObjArray *fVertexOutputArray;
+
+  ClassDef(VertexFinderDAClusterizerZT, 1)
+};
+
+#endif
