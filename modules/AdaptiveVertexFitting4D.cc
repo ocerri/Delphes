ef7c27c1b0ce253870c0bdc5f23ee799bc436fff
Olmo Cerri <olmo.cerri@gmail.com>
2018-02-20 11:58:58 -0800

    added vertex fitting and ingore compiled binaries

---
+++ modules/AdaptiveVertexFitting4D.cc
@@ -0,0 +1,260 @@
+/** \class AdaptiveVertexFitting4D
+ *
+ *  Cluster vertices from tracks using deterministic annealing and timing information
+ *
+ *  \authors O. Cerri
+ *
+ */
+
+
+#include "modules/AdaptiveVertexFitting4D.h"
+#include "classes/DelphesClasses.h"
+#include "classes/DelphesFactory.h"
+#include "classes/DelphesFormula.h"
+#include "classes/DelphesPileUpReader.h"
+
+#include "ExRootAnalysis/ExRootResult.h"
+#include "ExRootAnalysis/ExRootFilter.h"
+#include "ExRootAnalysis/ExRootClassifier.h"
+
+#include "TMath.h"
+#include "TString.h"
+#include "TFormula.h"
+#include "TRandom3.h"
+#include "TObjArray.h"
+#include "TDatabasePDG.h"
+#include "TLorentzVector.h"
+#include "TMatrixT.h"
+#include "TVector3.h"
+#include "TFitter.h"
+#include "TMinuit.h"
+// replace ln18 of makefile
+// LIBS = -lRooFitCore -lRooFit -lMinuit -lHtml -lPyROOT -lFoam -lRooStats -lTreePlayer -lTMVA
+// DELPHES_LIBS = $(shell $(RC) --libs) -lEG $(SYSLIBS) $(LIBS)
+
+#include "TAxis.h"
+#include "TGraphErrors.h"
+#include "TCanvas.h"
+#include "TString.h"
+#include "TLegend.h"
+#include "TFile.h"
+#include "TColor.h"
+#include "TLegend.h"
+
+#include <utility>
+#include <algorithm>
+#include <stdexcept>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+using namespace AVF_4D;
+
+//------------------------------------------------------------------------------
+// Global variable to pass info to the VertexFinderDAClusterizerZTVertexFinderDAClusterizerZT
+AdaptiveVertexFitting4D::tracks_t * global_tks;
+double global_z_v = 0;
+double global_chi2 = 0;
+
+
+//------------------------------------------------------------------------------
+
+AdaptiveVertexFitting4D::AdaptiveVertexFitting4D()
+{
+  fVerbose = 0;
+  fMaxIterations = 0;
+  fBetaMax = 0;
+  fCoolingFactor = 0;
+  fChi2_0 = 0;
+}
+
+//------------------------------------------------------------------------------
+
+AdaptiveVertexFitting4D::~AdaptiveVertexFitting4D()
+{
+}
+
+//------------------------------------------------------------------------------
+
+void AdaptiveVertexFitting4D::Init()
+{
+  fVerbose         = GetInt("Verbose", 0);
+
+  fMaxIterations   = GetInt("MaxIterations", 100);
+
+  fBetaMax         = GetDouble("BetaMax", 1.);
+
+  fCoolingFactor   = GetDouble("CoolingFactor", 0.8); // Multiply T so to cooldown must be <1
+  fChi2_0   = GetDouble("Chi2_0", 4); // Multiply T so to cooldown must be <1
+
+
+  fInputArray = ImportArray(GetString("InputArray", "VertexFinderDAClusterizerZT/vertices"));
+  fItInputArray = fInputArray->MakeIterator();
+
+  fOutputArray = ExportArray(GetString("OutputArray", "tracks"));
+  fVertexOutputArray = ExportArray(GetString("VertexOutputArray", "vertices"));
+}
+
+//------------------------------------------------------------------------------
+
+void AdaptiveVertexFitting4D::Finish()
+{
+  if(fItInputArray) delete fItInputArray;
+}
+
+//------------------------------------------------------------------------------
+
+void AdaptiveVertexFitting4D::Process()
+{
+  fItInputArray->Reset();
+  Candidate * mother;
+  while((mother = static_cast<Candidate*>(fItInputArray->Next())))
+  {
+    Candidate * cluster = static_cast<Candidate*>(mother->Clone());
+
+    vertex_t vtx(cluster->Position.T()/c_light, cluster->Position.Z());
+    if(fVerbose>2)
+    {
+      cout << endl << "-------------------- Vertex " << cluster->ClusterIndex << " --------------------------------"<< endl;
+      cout << Form("Start: {0, 0, %.2f +/- %.2f} mm - %.0f +/- %.0f ps", vtx.z, vtx.sigma_z, vtx.t, vtx.sigma_t) << endl;
+      cout << "N tracks: " << cluster->GetCandidates()->GetEntriesFast() << endl;
+    }
+
+    tracks_t tks;
+    fill(cluster, tks);
+
+    double beta = 0;
+
+    fit(beta, tks, vtx);
+
+    if(fVerbose>2)
+    {
+      cout << "chi2: " << vtx.chi2 << endl;
+      cout << Form("End: {0, 0, %.2f +/- %.2f} mm - %.2f +/- %.2f ps", vtx.z, vtx.sigma_z, vtx.t, vtx.sigma_t) << endl;
+    }
+
+    cluster->Position.SetT(vtx.t * c_light);
+    cluster->PositionError.SetT(vtx.sigma_t * c_light);
+
+    cluster->Position.SetZ(vtx.z);
+    cluster->PositionError.SetZ(vtx.sigma_z);
+
+    TIter TrackIt(cluster->GetCandidates());
+    TrackIt.Reset();
+
+    Candidate *tr;
+    while((tr = static_cast<Candidate*>(TrackIt.Next())))
+    {
+      tr->InitialPosition.SetT(cluster->Position.T());
+      tr->InitialPosition.SetZ(cluster->Position.Z());
+
+      fOutputArray->Add(tr);
+    }
+
+    fVertexOutputArray->Add(cluster);
+  }
+
+}
+
+
+//------------------------------------------------------------------------------
+// Fill tks with the candidates associated to the vtx
+void AdaptiveVertexFitting4D::fill(Candidate* vtx, tracks_t &tks)
+{
+  // loop over tracks belonging to this vertex
+  TIter TrackIt(vtx->GetCandidates());
+  TrackIt.Reset();
+
+  Candidate *tr;
+  unsigned int i = 0;
+  while((tr = static_cast<Candidate*>(TrackIt.Next())))
+  {
+    double aux = 0;
+
+    tks.t_out.push_back( tr->Position.T()/c_light );
+    aux = tr->ErrorT / c_light;
+    tks.s2t_out.push_back( aux*aux );
+
+    tks.z_out.push_back(tr->Position.Z());
+    tks.s2z_out.push_back(tr->PositionError.Z());
+
+    tks.Pt.push_back(tr->Momentum.Pt());
+    tks.s2_Pt.push_back(tr->ErrorPT * tr->ErrorPT);
+
+    tks.ctg_theta.push_back(tr->CtgTheta);
+    tks.E.push_back( sqrt(tr->Mass*tr->Mass + tr->Momentum.Pt()*tr->Momentum.Pt()*(1+ tr->CtgTheta*tr->CtgTheta)) );
+    tks.beta_z.push_back( tks.Pt[i] * tks.ctg_theta[i] / tks.E[i] );
+    tks.M.push_back(tr->Mass);
+
+    // tks.wz.push_back( 1. );
+
+    tks.tt.push_back( tr );
+
+    // if(fVerbose > 20) tks.dump(i);
+    i++;
+  }
+
+  return;
+}
+
+// -----------------------------------------------------------------------------
+// Fit the vertex and update the vertex position
+void AdaptiveVertexFitting4D::fit(double beta, tracks_t &tks, vertex_t &vtx)
+{
+  TFitter minuit(2);
+  minuit.SetDefaultFitter("Minuit");
+  minuit.GetMinuit()->SetPrintLevel(-1);
+  minuit.SetFCN(NegativeLogLikelihood);
+
+  // Beta not used for the moment
+  minuit.SetParameter(0, "beta", 0, 0, -beta, beta);
+  minuit.FixParameter(0);
+
+  minuit.SetParameter(1, "t_v", vtx.t, vtx.sigma_t, 5000, -5000);
+  global_tks = &tks;
+
+  // minuit.SetPrintLevel(TMath::Sign(1, fVerbose)-1);
+  minuit.ExecuteCommand("MIGRAD", new double(0), 0);
+
+  vtx.t = minuit.GetParameter(1);
+  vtx.sigma_t = minuit.GetParError(1);
+
+  vtx.z = global_z_v;
+  vtx.sigma_z = 1./sqrt(tks.sum_wz);
+
+  vtx.chi2 = global_chi2;
+}
+
+// -----------------------------------------------------------------------------
+// Global Function for the minimization
+
+void NegativeLogLikelihood(Int_t&, Double_t*, Double_t&f, Double_t*par, Int_t )
+{
+  double t_v = par[1];
+  // double beta = par[0];
+
+  unsigned int nt = global_tks->getSize();
+  std::vector<double> z(nt);
+  std::vector<double> wz(nt);
+
+  global_z_v = 0;
+  global_tks->sum_wz = 0;
+  for(unsigned int i = 0; i< nt; i++)
+  {
+    z[i] = global_tks->z(i, t_v);
+    wz[i] = 1./global_tks->s2z(i, t_v);
+    // cout << Form("%d: %f +/- %f", i, z[i], sqrt(1./wz[i])) << endl;
+
+    global_z_v += wz[i]*z[i];
+    global_tks->sum_wz += wz[i];
+  }
+  global_z_v /= global_tks->sum_wz;
+
+  global_chi2 = 0;
+  for(unsigned int i = 0; i< nt; i++)
+  {
+    global_chi2 += (z[i] - global_z_v)*(z[i] - global_z_v) * wz[i];
+  }
+
+  f = global_chi2;
+}
---
+++ modules/AdaptiveVertexFitting4D.h
@@ -0,0 +1,164 @@
+#ifndef AdaptiveVertexFitting4D_h
+#define AdaptiveVertexFitting4D_h
+
+/** \class AdaptiveVertexFitting4D
+ *
+ *  Cluster vertices from tracks using deterministic annealing and timing information
+ *
+ *  Author O. Cerri
+ *
+ */
+
+#include "classes/DelphesModule.h"
+
+#include <vector>
+#include <iostream>
+
+#ifndef _c_light__
+#define _c_light__
+namespace AVF_4D
+{
+  static const Double_t c_light   = 2.99792458e-01; //[mm/ps]
+}
+#endif
+
+using namespace std;
+using namespace AVF_4D;
+
+class TObjArray;
+class TIterator;
+class Candidate;
+
+void NegativeLogLikelihood(Int_t&, Double_t *, Double_t&f, Double_t*par, Int_t );
+
+class AdaptiveVertexFitting4D: public DelphesModule
+{
+  public:
+
+    class tracks_t : public TObject
+    {
+      public:
+        std::vector<double> t_out;      // t-coordinate at point of of intersection with the timing layer  [ps]
+        std::vector<double> s2t_out;      // sigma square of t_out [ps]
+
+        std::vector<double> z_out;      // z-coordinate at point of intersection with the timing layer [mm]
+        std::vector<double> s2z_out;      // sigma square of z_out [mm]
+
+        std::vector<double> Pt;    // Transverse Momentum
+        std::vector<double> s2_Pt;    // sigma square of transverse Momentum
+
+        std::vector<double> ctg_theta;    // CtgTheta = PZ/PT
+        std::vector<double> E;    // Energy
+        std::vector<double> beta_z;    // Beta along z
+        std::vector<double> M;
+
+        // parameters from the last iteration
+        // std::vector<double> wz;     // track weight
+        double sum_wz;     // track weight
+
+
+        std::vector<Candidate*> tt; // a pointer to the Candidate Track
+
+        // parameters from the last iteration
+
+        tracks_t(){}
+        ~tracks_t(){}
+
+        void dump(unsigned int i)
+        {
+          cout << "Track " << i << endl;
+          cout <<
+          Form("t_out: %1.2e  ", t_out[i]) <<
+          Form("st_out: %1.2e  ", sqrt(s2t_out[i])) <<
+          Form("z_out: %1.2e  ", z_out[i]) <<
+          Form("sz_out: %1.2e  ", sqrt(s2z_out[i])) <<
+          Form("Pt: %1.2e  ", Pt[i]) <<
+          Form("s2_Pt: %1.2e  ", sqrt(s2_Pt[i])) <<
+          Form("ctg_theta: %1.2e  ", ctg_theta[i]) <<
+          Form("E: %1.2e  ", E[i]) <<
+          Form("beta_z: %1.2e  ", beta_z[i]) <<
+          Form("M: %1.2e  ", M[i]) << endl << endl;
+        }
+
+        unsigned int getSize()
+        {
+          return z_out.size();
+        }
+
+        double z(unsigned int i, double t)
+        {
+          return z_out[i] + c_light * beta_z[i] * (t - t_out[i]);
+        }
+
+        double s2z(unsigned int i, double t)
+        {
+          // Solve! It overestimate the error
+          double out = 0;
+          out += s2z_out[i];
+
+          double aux = c_light * beta_z[i];
+          // out += aux * aux * s2t_out[i];
+
+          aux = c_light * (t - t_out[i]) * ctg_theta[i] * ( 1 - Pt[i]*Pt[i]*(1+ctg_theta[i]*ctg_theta[i])/(E[i]*E[i]) ) / E[i];
+          out += aux*aux*s2_Pt[i];
+
+          return out;
+        }
+    };
+
+    class vertex_t : public TObject
+    {
+      public:
+        double x,y,z,t;
+        double sigma_t = 30;
+        double sigma_z = 1;
+
+        double chi2 = 0;
+
+        vertex_t( double t_new, double z_new)
+        {
+          x = 0;
+          y = 0;
+          t = t_new;
+          z = z_new;
+        };
+
+        ~vertex_t(){};
+    };
+
+    AdaptiveVertexFitting4D();
+    ~AdaptiveVertexFitting4D();
+
+    void Init();
+    void Process();
+    void Finish();
+
+
+    // Fill the tracks structure from the vertex
+    void fill(Candidate* vtx, tracks_t &tks);
+
+    // Fit the vertex and update the vertex position
+    void fit(double beta, tracks_t &tks, vertex_t &vtx);
+
+  private:
+
+    UInt_t fVerbose;
+
+    UInt_t fMaxIterations;
+
+    Float_t fBetaMax;
+
+    Double_t fCoolingFactor;
+    Double_t fChi2_0;
+
+
+    TObjArray *fInputArray;
+    TIterator *fItInputArray;
+
+    TObjArray *fOutputArray;
+    TObjArray *fVertexOutputArray;
+
+    ClassDef(AdaptiveVertexFitting4D, 1)
+};
+
+#endif
